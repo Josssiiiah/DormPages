810f794d870e98342c4e2d6ca222375ba9128c86
Josiah Griggs <josiahstanford8@gmail.com>
2024-07-01 13:07:14 -0500

    deleted more github stuff

--- app/routes/login/route.tsx
+++ app/routes/login/route.tsx
@@ -73,9 +73,6 @@ export default function LoginRoute() {
         <h1>
           Login with GitHub
         </h1>
-        <Button>
-          <a href="/github">Sign in with GitHub</a>
-        </Button>
         <h1>
           Login with Google
         </h1>
